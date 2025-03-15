241951cfd0f3d80feee7cf0984a33684e26a153f
CatrionaMcGlynn <catriona@mcglynn.me.uk>
2025-03-15 14:59:37 +0000

    0.66 Updated Player.kt - added createPairs fun

--- src/Player.kt
+++ src/Player.kt
@@ -1,48 +1,48 @@
 class Player(
-    var name: String, 
-    var w: Int = 0, 
-    var d: Int = 0, 
-    var l: Int = 0, 
-    var aa: Int = 0, 
-    var bbb: Int = 0, 
-    var ccc: Int = 0, 
-    var ddd: Int = 0, 
-    var op: List<Player> = mutableListOf(), 
+    var name: String,
+    var w: Int = 0,
+    var d: Int = 0,
+    var l: Int = 0,
+    var aa: Int = 0,
+    var bbb: Int = 0,
+    var ccc: Int = 0,
+    var ddd: Int = 0,
+    var op: List<Player> = mutableListOf(),
     var opop: List<Player> = mutableListOf()) {
-        
-        fun updateW() {
-            w += 1
-        }
 
-        fun updateD() {
-            d += 1
-        }
-
-        fun updateL() {
-            l += 1
-        }
-
-        fun outputWLD() : String {
-            return "$w/$l/$d"
-        }
-                
-        fun updateAA(toAdd: Int = 1) {
-            aa += toAdd
-        }
-
+    fun updateW() {
+        w += 1
     }
+
+    fun updateD() {
+        d += 1
+    }
+
+    fun updateL() {
+        l += 1
+    }
+
+    fun outputWLD() : String {
+        return "$w/$l/$d"
+    }
+
+    fun updateAA(toAdd: Int = 1) {
+        aa += toAdd
+    }
+
+}
 
 fun outputAA(player: Player): String {
     var sAA = player.aa.toString()
-    sAA = if (sAA.length == 2) sAA else "0" + sAA
+    sAA = if (sAA.length == 2) sAA else "0$sAA"
     return sAA
 }
 
 fun outputXXX(iXXX: Int) : String {
     var sXXX = iXXX.toString()
     sXXX = when(sXXX.length) {
-        1 -> "00" + sXXX
-        2 -> "0" + sXXX
+        1 -> "00$sXXX"
+        2 -> "0$sXXX"
         else -> sXXX
     }
     return sXXX
@@ -61,8 +61,13 @@ fun outputDDD(player: Player): String {
 }
 
 fun outputAABBBCCCDDD(player: Player) : String {
-    var tiebreaker = outputAA(player) + outputBBB(player) + outputCCC(player) + outputDDD(player)
+    val tiebreaker = outputAA(player) + outputBBB(player) + outputCCC(player) + outputDDD(player)
     return tiebreaker
+}
+
+fun createPairs(players: List<Player>): List<List<Player>> {
+    val pairs = players.chunked(2)
+    return pairs
 }
 
 fun updateOnGameEnd(playerOne: Player, playerTwo: Player) {
@@ -87,14 +92,11 @@ fun updateOnGameDraw(playerOne: Player, playerTwo: Player) {
 }
 
 fun calculateAvgWinRate(opponents: List<Player>): Int {
-    var count: Int = 0
-    var winRate: Int = 0
-    var avgWinRate: Int = 0
+    var winRate = 0
     for (opponent in opponents) {
-        count += 1
         winRate += 1000 * opponent.w/(opponent.w + opponent.d + opponent.l)
-        avgWinRate = winRate/count
     }
+    val avgWinRate = winRate/opponents.size
     return if (avgWinRate == 1000) 999 else avgWinRate
 }
 
@@ -115,18 +117,19 @@ fun main() {
     val player3 = Player("Cat")
     val player4 = Player("Curie")
     val players = mutableListOf(player1, player2, player3, player4)
-    updateOnGameWin(player1, player2)
-    updateOnGameWin(player3, player4)
+    val pairs = createPairs(players)
+    updateOnGameWin(pairs[0][0], pairs[0][1])
+    updateOnGameWin(pairs[1][0], pairs[1][1])
     updateOnRoundEnd(players)
     updateOnGameWin(player1, player4)
     updateOnGameDraw(player2, player3)
     updateOnRoundEnd(players)
-    var p1Tiebreaker = outputAABBBCCCDDD(player1)
-    var p2Tiebreaker = outputAABBBCCCDDD(player2)
-    var p3Tiebreaker = outputAABBBCCCDDD(player3)
-    var p4Tiebreaker = outputAABBBCCCDDD(player4)
-    println("${player1.name}:   ${player1.aa} " + player1.outputWLD() + " ${p1Tiebreaker}")
-    println("${player3.name}:    ${player3.aa} " + player3.outputWLD() + " ${p3Tiebreaker}")
-    println("${player2.name}: ${player2.aa} " + player2.outputWLD() + " ${p2Tiebreaker}")
-    println("${player4.name}:  ${player4.aa} " + player4.outputWLD() + " ${p4Tiebreaker}")
+    val p1Tiebreaker = outputAABBBCCCDDD(player1)
+    val p2Tiebreaker = outputAABBBCCCDDD(player2)
+    val p3Tiebreaker = outputAABBBCCCDDD(player3)
+    val p4Tiebreaker = outputAABBBCCCDDD(player4)
+    println("${player1.name}:   ${player1.aa} " + player1.outputWLD() + " $p1Tiebreaker")
+    println("${player3.name}:    ${player3.aa} " + player3.outputWLD() + " $p3Tiebreaker")
+    println("${player2.name}: ${player2.aa} " + player2.outputWLD() + " $p2Tiebreaker")
+    println("${player4.name}:  ${player4.aa} " + player4.outputWLD() + " $p4Tiebreaker")
 }
