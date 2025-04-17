80e23b89de98ba168d41cdd36054bc362b48eca0
CatrionaMcGlynn <catriona@mcglynn.me.uk>
2025-04-17 21:13:43 +0100

    0.7.8 added bye fun

--- app/src/main/kotlin/com/themegatree/Player.kt
+++ app/src/main/kotlin/com/themegatree/Player.kt
@@ -72,10 +72,10 @@ fun outputAABBBCCCDDD(player: Player) : String {
     return tiebreaker
 }
 
-fun Player.outputTiebreaker(): Int {
-    val sTiebreaker = aa.toString() + bbb.toString() + ccc.toString() + ddd.toString()
-    val iTiebreaker = sTiebreaker.toInt()
-    return iTiebreaker
+fun Player.outputTiebreaker(): Long {
+    val sTiebreaker = outputAABBBCCCDDD(this)
+    val lTiebreaker = sTiebreaker.toLong()
+    return lTiebreaker
 }
 
 fun createPairs(players: List<Player>): List<List<Player>> {
@@ -84,8 +84,16 @@ fun createPairs(players: List<Player>): List<List<Player>> {
 }
 
 fun List<Player>.orderPlayers(): List<Player> {
-    val orderedPlayers = sortedBy { it.outputTiebreaker() }
+    val orderedPlayers = sortedByDescending { it.outputTiebreaker() }
     return orderedPlayers
+}
+
+fun bye(size: Int, player: Player, byes: MutableList<Player>) {
+    if (size % 2 != 0) {
+        byes.add(player)
+        player.updateW(1)
+        player.updateAA(3)
+    } 
 }
 
 fun updateOnGameEnd(playerOne: Player, playerTwo: Player) {
@@ -112,9 +120,18 @@ fun updateOnGameDraw(playerOne: Player, playerTwo: Player) {
 fun calculateAvgWinRate(opponents: List<Player>): Int {
     var winRate = 0
     for (opponent in opponents) {
-        winRate += 1000 * opponent.w/(opponent.w + opponent.d + opponent.l)
+        try {
+            winRate += 1000 * opponent.w/(opponent.w + opponent.d + opponent.l)
+        } catch (e: ArithmeticException) {
+            println("${e.message}")
+        }
     }
-    val avgWinRate = winRate/opponents.size
+    var avgWinRate = 0
+    try {
+        avgWinRate = winRate/opponents.size
+    } catch (e: ArithmeticException) {
+        println("No opponents. Error: ${e.message}")
+    }
     return if (avgWinRate == 1000) 999 else avgWinRate
 }
 
@@ -134,21 +151,25 @@ fun main() {
     val player2 = Player("Casper")
     val player3 = Player("Cat")
     val player4 = Player("Curie")
-    val players = mutableListOf(player1, player2, player3, player4)
+    val player5 = Player("Lulu")
+    val players = mutableListOf(player1, player2, player3, player4, player5)
     var pairs = createPairs(players)
+    var byes: MutableList<Player> = mutableListOf()
+    bye(players.size, pairs.last().get(0), byes)
     updateOnGameWin(pairs[0][0], pairs[0][1])
     updateOnGameWin(pairs[1][0], pairs[1][1])
     updateOnRoundEnd(players)
     pairs = createPairs(players.orderPlayers())
-    updateOnGameDraw(pairs[0][1], pairs[0][1])
+    bye(players.size, pairs.last().get(0), byes)
+    updateOnGameDraw(pairs[0][0], pairs[0][1])
     updateOnGameWin(pairs[1][0], pairs[1][1])
     updateOnRoundEnd(players)
     val p1Tiebreaker = outputAABBBCCCDDD(player1)
     val p2Tiebreaker = outputAABBBCCCDDD(player2)
     val p3Tiebreaker = outputAABBBCCCDDD(player3)
     val p4Tiebreaker = outputAABBBCCCDDD(player4)
-    println("${player1.outputName()}:   ${player1.aa} " + player1.outputWLD() + " $p1Tiebreaker")
     println("${player3.outputName()}:    ${player3.aa} " + player3.outputWLD() + " $p3Tiebreaker")
+    println("${player1.outputName()}:   ${player1.aa} " + player1.outputWLD() + " $p1Tiebreaker")
     println("${player4.outputName()}:  ${player4.aa} " + player4.outputWLD() + " $p4Tiebreaker")
     println("${player2.outputName()}: ${player2.aa} " + player2.outputWLD() + " $p2Tiebreaker")
 
